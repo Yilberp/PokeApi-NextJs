5a22ad5adb3fd710be756e9eb6b8f0cde379d998
Yilber Patiño <yilberesaupm@ufps.edu.co>
2022-09-29 16:17:29 -0500

    Implements ISR and ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -117,7 +117,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map((name) => ({
       params: { name },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
@@ -129,9 +129,18 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
-
+  const pokemon = await getPokemonInfo(name);
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
   return {
-    props: { pokemon: await getPokemonInfo(name) },
+    props: { pokemon },
+    revalidate: 86400,
   };
 };
 
